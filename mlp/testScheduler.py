0cda7c26a512b97f0aec79a33a4d98e1b8c9ac61
Liuwenyang <liuwenyang45@gmail.com>
2018-10-23 13:26:40 +0100

    提交

---
+++ mlp/testScheduler.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+import numpy as np
+import logging
+from mlp.data_providers import MNISTDataProvider, EMNISTDataProvider
+
+from mlp.layers import AffineLayer, SoftmaxLayer, SigmoidLayer, ReluLayer, LeakyReluLayer
+from mlp.errors import CrossEntropySoftmaxError
+from mlp.models import MultipleLayerModel
+from mlp.initialisers import ConstantInit, GlorotUniformInit
+from mlp.learning_rules import AdamLearningRule,GradientDescentLearningRule
+from mlp.optimisers import Optimiser
+from mlp.schedulers import CosineAnnealingWithWarmRestarts
+
+def train_model_and_plot_stats(
+        model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=True, scheduler=None):
+    # As well as monitoring the error over training also monitor classification
+    # accuracy i.e. proportion of most-probable predicted classes being equal to targets
+    data_monitors = {'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
+
+    # Use the created objects to initialise a new Optimiser instance.
+    optimiser = Optimiser(
+        model, error, learning_rule, train_data, valid_data, data_monitors, notebook=notebook, scheduler=scheduler)
+
+    # Run the optimiser for 5 epochs (full passes through the training set)
+    # printing statistics every epoch.
+    stats, keys, run_time = optimiser.train(num_epochs=num_epochs, stats_interval=stats_interval)
+
+    # Plot the change in the validation and training set error over training.
+    fig_1 = plt.figure(figsize=(8, 4))
+    ax_1 = fig_1.add_subplot(111)
+    for k in ['error(train)', 'error(valid)']:
+        ax_1.plot(np.arange(1, stats.shape[0]) * stats_interval,
+                  stats[1:, keys[k]], label=k)
+    ax_1.legend(loc=0)
+    ax_1.set_xlabel('Epoch number')
+
+    # Plot the change in the validation and training set accuracy over training.
+    fig_2 = plt.figure(figsize=(8, 4))
+    ax_2 = fig_2.add_subplot(111)
+    for k in ['acc(train)', 'acc(valid)']:
+        ax_2.plot(np.arange(1, stats.shape[0]) * stats_interval,
+                  stats[1:, keys[k]], label=k)
+    ax_2.legend(loc=0)
+    ax_2.set_xlabel('Epoch number')
+
+    return stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
+
+
+
+
+# Seed a random number generator
+seed = 11102018
+rng = np.random.RandomState(seed)
+batch_size = 100
+# Set up a logger object to print info about the training run to stdout
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.handlers = [logging.StreamHandler()]
+
+# Create data provider objects for the MNIST data set
+train_data = EMNISTDataProvider('train', batch_size=batch_size, rng=rng)
+valid_data = EMNISTDataProvider('valid', batch_size=batch_size, rng=rng)
+
+
+
+
+#setup hyperparameters
+learning_rate = 0.01
+num_epochs = 100
+stats_interval = 1
+input_dim, output_dim, hidden_dim = 784, 47, 100
+
+weights_init = GlorotUniformInit(rng=rng)
+biases_init = ConstantInit(0.)
+model = MultipleLayerModel([
+    AffineLayer(input_dim, hidden_dim, weights_init, biases_init),
+    ReluLayer(),
+    AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init),
+    ReluLayer(),
+    AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init),
+    ReluLayer(),
+    AffineLayer(hidden_dim, output_dim, weights_init, biases_init)
+])
+
+sgd_scheduler = CosineAnnealingWithWarmRestarts(min_learning_rate=0.0001, max_learning_rate=0.01,
+                                                       total_iters_per_period=25,
+                                                       max_learning_rate_discount_factor=0.9,
+                                                       period_iteration_expansion_factor=3.0)
+
+error = CrossEntropySoftmaxError()
+
+# Use a basic gradient descent learning rule
+learning_rule = GradientDescentLearningRule(learning_rate=learning_rate)
+
+#Remember to use notebook=False when you write a script to be run in a terminal
+_ = train_model_and_plot_stats(
+    model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=False,scheduler=sgd_scheduler)
